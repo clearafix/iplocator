6c552d2c292da149d73d48f0064307f6bc127340
BBK-PiJ-2015-87 <ivanovs223@gmail.com>
2017-07-31 00:41:31 +0100

    Find max(broadcast) IP if CIDR subnet provided

--- src/main/scala/utils/IpUtils.scala
+++ src/main/scala/utils/IpUtils.scala
@@ -8,4 +8,37 @@ object IpUtils {
     val arr = ip.split("\\.") map (_.toLong)
     (arr(0) << 24) + (arr(1) << 16) + (arr(2) << 8) + arr(3)
   }
+
+  def asString(arr: Array[Byte]): String = {
+    arr map (x => x & 0xff) mkString(".")
+  }
+
+  def invMaskFromSuffix(bits:String): Array[Byte]  = {
+    val mask = ~ (0xffffffff << (32 - bits.toByte))
+    Array[Byte]((mask >>> 24).asInstanceOf[Byte], (mask >> 16 & 0xff).asInstanceOf[Byte], (mask >> 8 & 0xff).asInstanceOf[Byte], (mask & 0xff).asInstanceOf[Byte])
+  }
+
+  def maskFromSuffix(bits:String): Array[Byte]  = {
+    val mask = 0xffffffff << (32 - bits.toByte)
+    Array[Byte]((mask >>> 24).asInstanceOf[Byte], (mask >> 16 & 0xff).asInstanceOf[Byte], (mask >> 8 & 0xff).asInstanceOf[Byte], (mask & 0xff).asInstanceOf[Byte])
+  }
+
+  /**
+    * Calculates broadcast IP address from CIDR notation string
+    * @param prefix
+    * @param suffix
+    * @return
+    */
+  def getBroadcast(prefix: String, suffix:String): Array[Byte] = {
+    val ip = toByteArray(prefix)
+    val mask = invMaskFromSuffix(suffix)
+    ip zip mask map (x => (x._1 | x._2).asInstanceOf[Byte])
+  }
+
+  def toBinaryString64(num: Long): String ={
+    val string = num.toBinaryString
+    val left = 64 - string.size
+    val zeros = new String(Array.fill(left)('0'))
+    new String(zeros) + string
+  }
 }
--- src/test/scala/utils/IpUtilsTest.scala
+++ src/test/scala/utils/IpUtilsTest.scala
@@ -1,16 +1,16 @@
 package utils
 
 import org.scalatest.{Matchers, WordSpec}
+import utils.IpUtils._
 
-class IpUtilsTest extends WordSpec with Matchers {
+class IpUtilsTest extends WordSpec with Matchers  with Timing{
   "IpUtilsTest" should {
     "should parse IP to byte array correctly" in {
       val ip = "0.120.130.255"
       val expected = Array[Byte](0, 120, 130.toByte, 255.toByte)
 
-      IpUtils.toByteArray(ip) should equal(expected)
+      toByteArray(ip) should equal(expected)
     }
-
     "should parse IP to long correctly" in {
       val ip0 = "0.0.0.0"
       val ip1 = "0.0.0.255"
@@ -23,6 +23,13 @@ class IpUtilsTest extends WordSpec with Matchers {
       IpUtils.toLong(ip2) should equal(65535L)
       IpUtils.toLong(ip3) should equal(16777215L)
       IpUtils.toLong(ip4) should equal(4294967295L)
+    }
+    "should return broadcast(max) IP address for subnet" in {
+      val ip0 = "1.0.0.0/24"
+      asString(millis{getBroadcast("1.0.0.0", "24")}) should equal ("1.0.0.255")
+      asString(millis{getBroadcast("1.0.0.0", "12")}) should equal ("1.15.255.255")
+      asString(millis{getBroadcast("1.0.0.0","8")}) should equal ("1.255.255.255")
+      asString(millis{getBroadcast("1.0.0.0", "31")}) should equal ("1.0.0.1")
     }
   }
 }
